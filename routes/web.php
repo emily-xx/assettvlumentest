03777fa229bb0327e37deac6a134e93966ee98b3
Emma Chatley <naomi.wind@gmail.com>
2017-10-30 17:28:44 +0000

    Removing old, adding new

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+include_once('../getID3-1.9.15/getid3/getid3.php');
+use Psr\Http\Message\ServerRequestInterface;
+use Illuminate\Http\Response;
+
+$router->post('api/v{version}/uploadvideo', function($version, ServerRequestInterface $request) { // Can create if/else tree later to utilise $version if required
+    if ($request->hasFile('video') && $request->file('video')->isValid()) { // Check for video file in request and that it uploaded correctly
+        $videoFile = $request->file('video');
+
+        $videoFile->store('videos'); // Save video
+
+        return response()->json(getID3($videoFile));
+    } else { // Handle bad request/failed upload
+        return response()->json(['error' => 'invalidFile']); // Handle on client-side via JavaScript to determine failed response
+    }
+});
